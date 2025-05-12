13bfb05ac0517c8e56b1489e7e5b64f928b2aa73
despkontopoulou <despkontopoulou@gmail.com>
2025-05-12 18:44:40 +0300

    login page implementation

---
+++ src/components/Header/SignInLogo.jsx
@@ -0,0 +1,9 @@
+export default function SignInLogo() {
+    return (
+        <img
+            src="/illustrations/sign-in-text.svg"
+            alt="CodeQuest Welcome Banner"
+            style={{ maxWidth: '100%', height: 'auto' }}
+        />
+    );
+}
